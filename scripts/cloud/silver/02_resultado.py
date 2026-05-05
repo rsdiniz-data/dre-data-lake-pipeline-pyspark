3a5db4d4a273d1609988b953852cdf85e9be5354
rsdiniz-data <rsdiniz@gmail.com>
2026-05-05 14:36:46 -0300

    Update 02_resultado.py

--- scripts/cloud/silver/02_resultado.py
+++ scripts/cloud/silver/02_resultado.py
@@ -9,7 +9,7 @@
 #
 # 🔗 Rastreabilidade:
 # - Documento técnico: ../docs/03_desenvolvimento.md
-# - Documentação do projeto/artigo: ../docs/anexos/documentacao_dre_cloud.docx
+# - Documento funcional/artigo ../docs/anexos/documento_funcional.docx
 #   3.4 Desenvolvimento dos notebooks em PySpark
 #   3.4.2 Notebook 02 – Transformações de Resultado
 #
